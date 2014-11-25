edb186114cfb13ab161d0b0f366b25c3cf2e8a1a
Jackson Hong <jacksonxxhong@gmail.com>
2014-11-25 18:46:05 -0500

    init commit for heap implentation

---
+++ data_structures/heap.py
@@ -0,0 +1,12 @@
+"""
+More explicitly, this is an implementation of a binary min-heap. Using an array as the data structure.
+"""
+class Heap:
+    def __init__(self):
+        self.heap = []
+
+
+
+if __name__ == '__main__':
+    h = Heap()
+    print h.heap
